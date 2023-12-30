16f54d1af4070a73572f738f4102f111b47b0b47
migue <miguelangelosilvacarvalho96@gmail.com>
2023-12-30 14:55:26 +0000

    Handling captured pieces
    Checklist:
     Method UI.PrintCapturedPieces
     Update UI.PrintMatch
     Update Program logic
     Lists in ChessMatch: _piecesOnTheBoard, _capturedPieces
    o Update constructor
    o Update PlaceNewPiece
    o Update MakeMove
     OOP Topics:
    o Encapsulation
    o Constructors
     Data Structures Topics:
    o List

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,12 +6,18 @@ import src.boardgame.Position;
 import src.chess.pieces.King;
 import src.chess.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
     private int turn;
     private Color currentPlayer;
-
-
     private Board board;
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
+
+
+
 
     public ChessMatch() {
         board = new Board(8,8);
@@ -89,11 +95,16 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
     private  void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
     private void initialSetup(){
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
